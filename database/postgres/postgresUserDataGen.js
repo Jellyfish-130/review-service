3b8105445b08a7d7b9438d488ef613772e391e75
JacobDeCastroJohnson <Jacob.DeCastro.J@gmail.com>
2020-10-17 19:04:15 -0700

    Initialize Postgres Users Data Gen file with dependencies

---
+++ database/postgres/postgresUserDataGen.js
@@ -0,0 +1,9 @@
+//Incorporate Dependencies
+// const db = require('./postgres.sql');
+const fs = require('fs');
+const faker = require('faker');
+const csvWriter = require('csv-write-stream');
+const writer = csvWriter();
+
+
+
