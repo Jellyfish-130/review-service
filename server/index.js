4e21aef56ba21411220f5e5538a5106ff230ce36
JacobDeCastroJohnson <Jacob.DeCastro.J@gmail.com>
2020-10-25 21:48:37 -0700

    Update Post Request for POSTMAN handling

--- server/index.js
+++ server/index.js
@@ -1,19 +1,92 @@
+require('newrelic');
 const express = require('express');
 const path = require('path');
 const app = express();
 const client = require('../database/postgres/index.js');
+const bodyParser = require('body-parser')
 
 const port = 3003;
 
 // app.use(express.static(__dirname + '/../client/dist'));
 
-// app.get('/', (req, res) => {
-//   res.send('Hello World!')
-// });
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json())
+
+// ==========  GET ALL REVIEWS FOR SPECIFIC ROOM ==========
+// --> /api/rooms/:roomId/reviews
+app.get('/api/rooms/:roomId/reviews', (req, res) => {
+  const { roomId } = req.params;
+  console.log('Get All Rooms input: ', roomId);
+
+   const getAllReviews = `SELECT * FROM reviews WHERE room_id = '${roomId}'`;
+   console.log('GET REQUEST TEST: ');
+   client.query(getAllReviews, (err, results) => {
+     if (err) {
+       res.status(400).send();
+     } else {
+       console.log('successful Get-All-Room-Reviews request from Server');
+       res.status(200).send(results)
+     }
+   })
+ })
+
+// ========= GET ALL REVIEWS FOR A SPECIFIC USER ============
+// --> /api/users/:userId/reviews
+app.get('/api/users/:userId/reviews', (req, res) => {
+ const { userId } = req.params;
+ console.log('Get All User Reviews input: ', userId);
+
+  const getAllReviews = `SELECT * FROM reviews WHERE user_id = '${userId}'`;
+  console.log('GET REQUEST TEST: ');
+  client.query(getAllReviews, (err, results) => {
+    if (err) {
+      res.status(400).send();
+    } else {
+      console.log('successful Get-All-User_Reviews request from Server');
+      res.status(200).send(results)
+    }
+  })
+})
+
+// ========== POST A REVIEW TO A SPECIFIC LISTING ==============
+// --> /api/rooms/:roomId/reviews
+app.post('/api/rooms/:roomId/reviews', (req, res) => {
+
+  console.log ('PARAM CHECK: ', req.params);
+  console.log('BODY CHECK: ', req.body);
+
+    const review_id = req.body.review_id;
+    const dateNum = req.body.dateNum;
+    const description = req.body.description;
+    const cleanlinessRating = req.body.cleanlinessRating;
+    const communicationRating = req.body.communicationRating;
+    const checkInRating = req.body.checkInRating;
+    const accuracyRating = req.body.accuracyRating;
+    const locationRating = req.body.locationRating;
+    const valueRating = req.body.valueRating;
+    const totalRating = req.body.totalRating;
+    const room_id = req.body.room_id;
+    const user_id = req.body.user_id;
 
 
-app.get('/', (req, res) => {
-  // res.send('Hello World!')
+  const postQuery = `INSERT INTO public."reviews" ("review_id", "dateNum", "description", "cleanlinessRating", "communicationRating", "checkInRating", "accuracyRating", "locationRating", "valueRating", "totalRating", "room_id", "user_id") VALUES ('${review_id}', '${dateNum}', '${description}', '${cleanlinessRating}', '${communicationRating}', '${checkInRating}', '${accuracyRating}', '${locationRating}', '${valueRating}', '${totalRating}', '${room_id}', '${user_id}')`
+
+   client.query(postQuery, (err, results) => {
+     if (err) {
+       console.log('Bad POST Request');
+       res.status(400).send();
+     } else {
+       console.log('Successful POST request from Server');
+       res.status(200).send()
+     }
+   })
+ })
+
+// =========== UPDATE A SPECIFIC REVIEW FOR A SPECIFIC LISTING ============
+// --> /api/rooms/:roomId/reviews/:reviewId
+app.patch('/', (req, res) => {
 
   const getAll = 'SELECT * FROM reviews limit 2';
   console.log('GET REQUEST TEST: ');
@@ -25,6 +98,27 @@ app.get('/', (req, res) => {
       res.status(200).send(results)
     }
   })
+})
+
+
+// ============== DELETE api/rooms/:roomId/reviews/:reviewId ==============
+app.delete('api/rooms/:roomId/reviews/:reviewId', (req, res) => {
+
+  const deleteCheck = req.params.roomId;
+  const deletePut = req.params.reviewId;
+
+  console.log('Delete check: ', deleteCheck, deletePut);
+
+  // const getAll = 'SELECT * FROM reviews limit 2';
+  // console.log('GET REQUEST TEST: ');
+  // client.query(getAll, (err, results) => {
+  //   if (err) {
+  //     res.status(400).send();
+  //   } else {
+  //     console.log('successful GetAll request from Server');
+  //     res.status(200).send(results)
+  //   }
+  // })
 })
 
 app.listen(port, () => {
